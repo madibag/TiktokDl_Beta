e336f69670e91e2ce043ef376652caf47ef1e610
madibag <62722024+madibag@users.noreply.github.com>
2020-10-06 12:38:48 +0300

    Update live.py

--- Tokbot/plugins/live.py
+++ Tokbot/plugins/live.py
@@ -9,16 +9,16 @@ from upload import Upload
 
 @Client.on_message(Filters.command(["live"]))
 async def live(client, message):
-	url = message.text.split("/live")[0]
-	link ,name = get_lnk(url)
-	name = name+"mkv"
-	cmd = ["ffmpeg1","-i",link,"-o",name]
-	process = await asyncio.create_subprocess_shell(
+    url = message.text.split("/live")[0]
+    link ,name = get_lnk(url)
+    name = name+"mkv"
+    cmd = ["ffmpeg1","-i",link,"-o",name]
+    process = await asyncio.create_subprocess_shell(
         cmd,
         stdout=asyncio.subprocess.PIPE,
         stderr=asyncio.subprocess.PIPE
     )
     stdout, stderr = await process.communicate()
-	if stdout:
-		return Upload(client,message,name)
+    if stdout:
+        return Upload(client,message,name)
 	
